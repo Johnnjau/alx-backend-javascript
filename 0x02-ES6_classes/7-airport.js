172283053d14f052a49538a84cac84bd5534f90a
John <johnnjau202@gmail.com>
2024-05-31 00:08:26 +0300

    7-airport.js

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,18 @@
+/* eslint-disable */
+export default class Airport {
+  constructor(name, code) {
+    if (name.constructor !== String) {
+      throw new TypeError('Name must be a number');
+    }
+    this._name = name;
+
+    if (code.constructor !== String) {
+      throw new TypeError('Code must be a number');
+    }
+    this._code = code;
+  }
+
+  get [Symbol.toStringTag]() {
+    return this._code;
+  }
+}
